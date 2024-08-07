7ff9bf2a865da1e4f1f9606a01a1770ec91e3e8a
Talibouyadanfa7 <stg_danfa75294@orange-sonatel.com>
2024-08-07 23:38:31 +0000

    add test

--- run_olist_segmentation.sh
+++ run_olist_segmentation.sh
@@ -15,27 +15,7 @@ mkdir -p "${LOGS_DIR}"
 # Message de début d'exécution
 echo "================================== Start olist segmentation ====================================="
 
-# Exécuter le notebook de nettoyage et d'EDA
-papermill notebooks/e-commerce-cleaning-and-eda.ipynb \
-"${LOGS_DIR}/${EXECUTION_DATE}-e-commerce-cleaning-and-eda.ipynb" \
--k python39 --report-mode --log-output --no-progress-bar
 
-# Vérifier si la première exécution a échoué
-if [ $? != 0 ]; then
-  echo "ERROR: failure during e-commerce-cleaning-and-eda execution!"
-  exit 1
-fi
-
-# Exécuter le notebook de segmentation des clients
-papermill notebooks/e-commerce-customers-segmentation.ipynb \
-"${LOGS_DIR}/${EXECUTION_DATE}-e-commerce-customers-segmentation.ipynb" \
--k python39 --report-mode --log-output --no-progress-bar
-
-# Vérifier si la deuxième exécution a échoué
-if [ $? != 0 ]; then
-  echo "ERROR: failure during segmentation!"
-  exit 1
-fi
 
 # Message de succès
 echo "================================ SUCCESS: Customer segmentation Done ==================================="
